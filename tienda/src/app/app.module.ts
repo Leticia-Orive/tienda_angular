af2dad5006cb451648755c5b3ad2c6c8fc5f461b
Leticia Orive <leticia.orive.contractor@bbva.com>
2023-08-28 11:14:50 +0200

    instalacion bootstrap

--- tienda/src/app/app.module.ts
+++ tienda/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, RouterModule.forRoot([])],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot([]), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
